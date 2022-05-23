eaf6a485f46bc977c62d63dcc6b6e71acfd8e1a5
Maxime91680 <m_commaret@orange.fr>
2022-05-23 15:09:27 +0200

    Bouton

--- src/application/View/ControllerLogin.java
+++ src/application/View/ControllerLogin.java
@@ -1,31 +1,30 @@
 package application.View;
 
-import java.awt.event.ActionEvent;
+
 import java.io.IOException;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import javafx.scene.Node;
 
 public class ControllerLogin {
+		private Stage stage;
+		private Scene scene;
+		private Parent root;
 	
-	  @FXML
-	    private Button btnValider;
-	  	private Stage stage;
-	  	private Scene scene;
-	  	private Parent root;
-	  
-	  	void Validation(ActionEvent event) throws IOException {
-	  		root = FXMLLoader.load(getClass().getResource("../View/PageAcceuil.fxml"));
-	  		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-	  		scene = new Scene(root);
-	  		stage.setScene(scene);
-	  		stage.show();
-	  }
-	 
+
+	    @FXML
+	    void Display(ActionEvent event) throws IOException {
+	    	root = FXMLLoader.load(getClass().getResource("PageAccueil.fxml"));
+			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+	    	scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+	    }
+
 
 }
