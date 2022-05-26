732c3a278d14e42a91e0a7be88ede8d6b7427319
Maxime91680 <m_commaret@orange.fr>
2022-05-26 21:47:14 +0200

    Amelioration

--- src/application/Controller/Main.java
+++ src/application/Controller/Main.java
@@ -7,16 +7,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
-import com.mysql.jdbc.PreparedStatement;
-
-
 public class Main extends Application {
-	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -27,26 +18,6 @@ public class Main extends Application {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-		
-		 try
-	        {
-	            Class.forName("com.mysql.jdbc.Driver");
-	            Connection con=DriverManager.getConnection(
-	                    "jdbc:mysql://localhost:3306/periscolaire?characterEncoding=latin1","root","Jmay1994Tahi-ti1");
-	            Statement stmt=con.createStatement();
-	            ResultSet rs=stmt.executeQuery("show databases;");
-	            
-	         // create a Statement from the connection
-	            Statement statement = con.createStatement();
-
-	            // insert the data
-	            statement.executeUpdate("INSERT INTO Customers " + "VALUES (1001, 'Simpson', 'Mr.', 'Springfield', 2001)");
-	            System.out.println("Connected");
-	        }
-	        catch(Exception e)
-	        {
-	            System.out.println(e);
-	        }
 	}
 	
 	public static void main(String[] args) {
